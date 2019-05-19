ab24fb77e4ca55d8e1a780dc7a10a767f6f0275d
Seb <sebdevine@users.noreply.github.com>
2019-05-19 19:23:55 +0200

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
 
-docker run -it -p8888:8888 -v c:/users/seb/Onedrive/Code/Notebooks:/home/foobar/notebooks -t ihaskell:seb 
+docker run -it -p8888:8888 -v c:/users/seb/Onedrive/Code/Notebooks:/home/foobar/notebooks -t ihaskell:latest 
